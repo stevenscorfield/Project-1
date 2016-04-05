3d87ef6ff109684170ebbf9ab9206bc2e1317e5f
Steven Scorfield <steven.scorfield@gmail.com>
2016-04-05 16:15:05 +0100

    Marine Tank Calulator Site

---
+++ MarineTankCalculations/MarineTankCalculations/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarineTankCalculations.MvcApplication" Language="C#" %>
---
+++ MarineTankCalculations/MarineTankCalculations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarineTankCalculations.Startup))]
+namespace MarineTankCalculations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
